17ae8f54645172c6b3e4abb651a50f33a5cad4da
Ian Moore <ian.mark.moore@gmail.com>
2017-11-23 19:40:33 -0600

    add keyboard support

--- src/Main.elm
+++ src/Main.elm
@@ -4,14 +4,36 @@ module Main exposing (update, subscriptions, init)
 --http://juliaheffernan.tumblr.com/post/30863290499/aaaaaaaaand-here-is-one-in-space
 
 import Html exposing (..)
+import Keyboard
 import Message exposing (..)
 import Model exposing (..)
 import View exposing (render)
 
 
+getDirectionForKeyCode : Keyboard.KeyCode -> Maybe DanceDirection
+getDirectionForKeyCode code =
+    case code of
+        37 -> Just UpLeft
+        39 -> Just UpRight
+        40 -> Just LeftRight
+        _ -> Nothing
+
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
     case Debug.log "msg" msg of
+        KeyDown code ->
+            case getDirectionForKeyCode code of
+                Just direction ->
+                    { model 
+                    | danceDirection = direction
+                    } ! []
+                Nothing ->
+                    model ! []
+        KeyUp code ->
+            { model 
+            | danceDirection = Down
+            } ! []
         NoOp ->
             model ! []
         UpdateDirection direction ->
@@ -22,7 +44,10 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.batch
+        [ Keyboard.downs KeyDown
+        , Keyboard.ups KeyUp
+        ]
 
 
 init : (Model, Cmd Msg)
--- src/Message.elm
+++ src/Message.elm
@@ -1,7 +1,10 @@
 module Message exposing (..)
 
+import Keyboard
 import Model exposing (DanceDirection)
 
 type Msg
     = NoOp
+    | KeyDown Keyboard.KeyCode
+    | KeyUp Keyboard.KeyCode
     | UpdateDirection DanceDirection
